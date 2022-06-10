a43c3eb2ae7b0d435aca8b1fe3830f95c06397fd
RMF-SIDE29 <96187407+windcycle@users.noreply.github.com>
2022-06-10 07:47:24 -0700

    Add files via upload

---
+++ shhh.py
@@ -0,0 +1,41 @@
+#Start
+from time import sleep 
+import sys
+a=str(input("Are you sure want to continue? Yes or No?: "))
+if(a=="y"):
+    print("Again [y/n]")
+    b=str(input("Make your choice: "))
+    if(b=="y"):
+      hacktheworld=""" Updating Packages installer...100%
+Session updated...100%
+Mostly done...100%
+OMG...
+NOT REALLY SURE...........................
+        SALE FOR CONTRACT 20
+        VIRUS DETECTED PLEASE CLEAN UP YOUR DEVICE!
+        
+        List your username:  List your password:
+        DIK124                 X90aa890
+        funway109             AM07DSA
+        salesforway            V09OIL13
+        
+                        ALMOST 
+                        
+        Please contact your owner:-----------------------------------------
+        
+        Damn no phone number
+        
+        
+>>>>>>>>>>>>>>> Packages installer success....>>>>>>>>>>>>>>
+  """
+    else:
+        print("terminated 0")
+        exit()
+else:
+    print("terminated 0")
+    exit()
+for letter in hacktheworld:
+    sleep(0.03)#typing text speed
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    # End Code
